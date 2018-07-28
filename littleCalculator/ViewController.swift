6260789dd5554a0abba3aa618c5bf041c3ad4c50
Jason Hsu <junchoon14@gmail.com>
2018-07-28 10:49:15 +0800

    7.27/2018 ver0

--- littleCalculator/ViewController.swift
+++ littleCalculator/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var firstNumberField: UITextField!
+    @IBOutlet weak var secondNumberField: UITextField!
+    @IBOutlet weak var operatorSelected: UISegmentedControl!
+    @IBAction func operatorCalculatieSegment(_ sender: UISegmentedControl) {
+        let calculator = operatorSelected.selectedSegmentIndex
+        let firstNumber = firstNumberField.text
+        let secondNumber = secondNumberField.text
+        
+        
+        switch calculator {
+        case 0:
+            if let firstNumber = firstNumber, let secondNumber = secondNumber {
+                let result:Int = Int(firstNumber)! + Int(secondNumber)!
+                answerLabel.text = "\(result)"
+            }
+        case 1:
+            if let firstNumber = firstNumber, let secondNumber = secondNumber {
+                let result:Int = Int(firstNumber)! - Int(secondNumber)!
+                answerLabel.text = "\(result)"
+            }
+        case 2:
+            if let firstNumber = firstNumber, let secondNumber = secondNumber {
+                let result:Int = Int(firstNumber)! * Int(secondNumber)!
+                answerLabel.text = "\(result)"
+            }
+        case 3:
+            if let firstNumber = firstNumber, let secondNumber = secondNumber {
+                let result:Int = Int(firstNumber)! / Int(secondNumber)!
+                answerLabel.text = "\(result)"
+            }
+        default:
+            break
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        firstNumberField.placeholder = "數字1"
+        secondNumberField.placeholder = "數字2"
+        answerLabel.text = "我是答案"
     }
 
 
