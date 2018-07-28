c60b1e925a7c6453e07aa6351e50f23ba235609e
Jason Hsu <junchoon14@gmail.com>
2018-07-28 09:20:55 +0800

    Initial Commit

---
+++ littleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  littleCalculator
+//
+//  Created by Jason Hsu on 2018/7/28.
+//  Copyright © 2018 junchoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
